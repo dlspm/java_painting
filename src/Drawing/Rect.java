845683606897b9c7d943f0631f6e93b795025884
dlspm <q0978298512@gmail.com>
2017-10-13 21:36:58 +0800

    Page_DrawRect OK
    Page_DrawRect OK

---
+++ src/Drawing/Rect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Drawing;
+import java.awt.Point;
+/**
+ *
+ * @author angus
+ */
+public class Rect {
+//    Point xy;
+//    Point y;
+//    int width;
+//    int height;
+//    Rect(Point a, int c, int d){
+//        xy = a;
+//        width = c;
+//        height = d;
+//    }
+    Point xy;
+    Rect(Point a){
+        xy = a;
+    }
+    
+}
