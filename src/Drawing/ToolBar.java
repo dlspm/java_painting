21002fb445e71bff6fc2275ce334658b4cdb3ed2
dlspm <q0978298512@gmail.com>
2017-12-03 20:43:23 +0800

    new Previous,Next Page feature

--- src/Drawing/ToolBar.java
+++ src/Drawing/ToolBar.java
@@ -18,6 +18,23 @@ public class ToolBar extends Panel{
         this.setBackground(Color.lightGray);
         this.setLayout(new FlowLayout());
         
+        
+        Button prevPageBtn = new Button("Previous Page");
+        this.add(prevPageBtn);
+        prevPageBtn.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                System.out.println(ep.curPage);
+                if(ep.curPage-1 > 0){
+                    ep.mainWin.remove(ep.activePage);
+                    ep.activePage = ep.pages.elementAt(ep.pages.indexOf(ep.activePage)-1);
+                    ep.mainWin.add(ep.activePage);
+                    ep.megBar.updateInfo(--ep.curPage, ep.numPages);
+                }
+            }
+        });
+        
         Button newPageBtn = new Button("New Page");
         this.add(newPageBtn);
         newPageBtn.addMouseListener(new MouseAdapter()
@@ -32,12 +49,26 @@ public class ToolBar extends Panel{
                 ep.activePage = new Page();
                 ep.pages.add(ep.activePage);
                 ep.mainWin.add(ep.activePage, BorderLayout.CENTER);
-                //ep.mainWin.setVisible(false);
                 ep.mainWin.setVisible(true);
-                
-                ep.numPages++;
-                ep.curPage++;
+                ep.curPage = ++ep.numPages;// 為了讓每次新增都確保回到最後面
+//                ep.numPages++;
+//                ep.curPage++;
                 ep.megBar.updateInfo(ep.curPage, ep.numPages);
+            }
+        });
+        
+        Button nextPageBtn = new Button("Next Page");
+        this.add(nextPageBtn);
+        nextPageBtn.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                if(ep.numPages<7 && ep.numPages>0 && ep.curPage < ep.numPages ){
+                    ep.mainWin.remove(ep.activePage);
+                    ep.activePage = ep.pages.elementAt(ep.pages.indexOf(ep.activePage)+1);
+                    ep.mainWin.add(ep.activePage);
+                    ep.megBar.updateInfo(++ep.curPage, ep.numPages);
+                } 
             }
         });
     }
